05392740e84f5d73c4efb02538c4c327335c1073
JyotiGhising1 <83853614+JyotiGhising1@users.noreply.github.com>
2024-05-22 15:34:43 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let leftPointer = 0 ; leftPointer < word.length / 2; leftPointer++){
+    let rightPointer = word.length - leftPointer - 1
+    if(word[leftPointer] != word[rightPointer]){
+      return false
+    }
+    else {
+      return true
+    }
+  }
 }
-
 /* 
   Add your pseudocode here
+  Write a function isPalindrome that will receive one argument, a string. Your function should return true if the string is a palindrome (that is, if it reads the same forwards and backwards, like "mom" or "racecar"), and return false if it is not a palindrome.
+  function isPalindrome(word)
+  for leftpointer FROM 0 to length of inputString / 2
+    SET rightPointer TO length of inputString - leftPointer -1
+    IF charcter at leftpointer is not equal to character at rightPointer 
+      RETURN FALSE
+    END FOR 
+
+    IF all charcter match 
+    RETURN true
+    END FUNCTION
 */
 
 /*
